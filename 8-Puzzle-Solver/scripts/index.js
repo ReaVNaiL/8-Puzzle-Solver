9051f0e74027c69a20be170ce2a8abe93bec13b3
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-10-05 18:23:28 -0400

    Added Styling For Griding And General Layout

---
+++ 8-Puzzle-Solver/scripts/index.js
@@ -0,0 +1,28 @@
+function shuffle_grid() {
+    var grid = document.getElementById("grid");
+
+    var grid_elements = grid.getElementsByTagName("div");
+    var grid_elements_array = Array.from(grid_elements);
+
+    grid_elements_array = shuffle(grid_elements_array);
+    for (var i = 0; i < grid_elements_array.length; i++) {
+        grid.appendChild(grid_elements_array[i]);
+    }
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+function solve_grid() {
+    // TODO: Implement this function
+}
