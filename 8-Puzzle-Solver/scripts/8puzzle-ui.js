06ae2e81e8c1130e863c2a9a0c12df4656f2985f
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-10-12 16:43:24 -0400

    Convert Adjacent From Dict to Array

--- 8-Puzzle-Solver/scripts/8puzzle-ui.js
+++ 8-Puzzle-Solver/scripts/8puzzle-ui.js
@@ -1,23 +1,28 @@
 function getAdjacentElements() {
-    var adjacentElements = {};
+    var adjacentElements = [];
+
     var gridArray = getCurrentGrid();
     var emptyTileIndex = gridArray.indexOf('0');
 
+    if (emptyTileIndex - 3 >= 0) {
+        adjacentElements.push(gridArray[emptyTileIndex - 3]);
+    }
+
     // Top element
     if (emptyTileIndex - 3 >= 0) {
-        adjacentElements.top = gridArray[emptyTileIndex - 3];
+        adjacentElements.push(gridArray[emptyTileIndex - 3]);
     }
     // Bottom element
     if (emptyTileIndex + 3 <= 8) {
-        adjacentElements.bottom = gridArray[emptyTileIndex + 3];
+        adjacentElements.push(gridArray[emptyTileIndex + 3]);
     }
     // Left element
     if (emptyTileIndex - 1 >= 0 && emptyTileIndex % 3 != 0) {
-        adjacentElements.left = gridArray[emptyTileIndex - 1];
+        adjacentElements.push(gridArray[emptyTileIndex - 1]);
     }
     // Right element
     if (emptyTileIndex + 1 <= 8 && emptyTileIndex % 3 != 2) {
-        adjacentElements.right = gridArray[emptyTileIndex + 1];
+        adjacentElements.push(gridArray[emptyTileIndex + 1]);
     }
 
     return adjacentElements;
@@ -26,14 +31,10 @@ function getAdjacentElements() {
 function isAdjacent(tileId) {
     var adjacentElements = getAdjacentElements();
 
-    for (var key in adjacentElements) {
-        if (adjacentElements.hasOwnProperty(key)) {
-            if (adjacentElements[key] == tileId) {
-                return true;
-            }
-        }
+    if (adjacentElements.includes(tileId)) {
+        return true;
     }
-
+    
     return false;
 }
 
